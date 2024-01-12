1ed052ca8033866b7fc831f5d70dec77639962dd
Jarrett Thompson <jarrett.thompson@alwaysai.co>
2024-01-11 16:14:21 -0800

    Add form validation to Submit component

--- src/app/_components/Submit.tsx
+++ src/app/_components/Submit.tsx
@@ -1,10 +1,42 @@
 "use client";
 import React from "react";
+import { useSelector } from "react-redux";
 import { Button } from "./ui/button";
 
 const Submit = () => {
+  const {
+    selectedAction,
+    item,
+    selectedSubAction,
+    selectedPrice,
+    selectedWhere,
+    selectedCity,
+  } = useSelector((state: any) => state.query);
+
+  const disableSubmit = () => {
+    if (
+      //TODO: Check for valid inputs
+      selectedAction == "" ||
+      item === "" ||
+      selectedSubAction === "" ||
+      selectedPrice === "" ||
+      selectedWhere === "" ||
+      selectedCity.name === ""
+    ) {
+      return true;
+    }
+    return false;
+  };
+
   const handleSubmit = () => {
-    console.log("submitted");
+    console.log("submitting...", {
+      selectedAction,
+      item,
+      selectedSubAction,
+      selectedPrice,
+      selectedWhere,
+      selectedCity,
+    });
   };
 
   return (
@@ -13,6 +45,7 @@ const Submit = () => {
         variant="outline"
         className="ml-16 border-green-500 bg-transparent px-16 text-green-500 hover:bg-blue-700 hover:bg-opacity-50 hover:text-white"
         onClick={handleSubmit}
+        disabled={disableSubmit()}
       >
         submit
       </Button>
