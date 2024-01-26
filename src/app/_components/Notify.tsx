3ce4de6dc039c10449191c0f47be5ea380991ec4
Jarrett Thompson <jarrett.thompson@alwaysai.co>
2024-01-26 12:34:00 -0800

    Fix Notify component rendering issue

--- src/app/_components/Notify.tsx
+++ src/app/_components/Notify.tsx
@@ -75,7 +75,7 @@ const Notify = () => {
             className="flex text-sm font-medium leading-none text-green-500 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
           >
             Notify me updates for this item every
-            <Select
+            {/* <Select
               value={notificationPeriod.toString()}
               onValueChange={setNotificationPeriod}
               noIcon={true}
@@ -91,7 +91,7 @@ const Notify = () => {
                   <SelectItem value="30">30</SelectItem>
                 </SelectGroup>
               </SelectContent>
-            </Select>{" "}
+            </Select>{" "} */}
             {notificationPeriod === "1" ? "day" : "days"}
           </label>
           {/* <p className="text-sm text-muted-foreground">
