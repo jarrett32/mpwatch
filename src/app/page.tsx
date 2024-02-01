aa9da4b10a2433570d398da01a09bc3cc6b282ee
jarrett32 <jarrettthompson32@gmail.com>
2024-02-01 13:04:04 -0800

    added announcements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,11 @@ import Submit from "./_components/Submit";
 export default async function Home() {
   return (
     <main className="min-h-screen flex-col bg-gradient-to-tl from-[#091d49] to-[#0c0c18] text-white">
+      <div className="flex w-full justify-center bg-slate-600 p-2 font-bold text-white">
+        ** If you are no facebook entries appear, either the city is not yet
+        supported or, facebook has changed their API. Please let us know if you
+        have any issues. **
+      </div>
       <div className="container flex max-w-6xl flex-col justify-center gap-12 px-4">
         <Header />
         <div className="flex w-full flex-col">
